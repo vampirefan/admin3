c001ff02b6f390a4f80843c1baf194e1f5ffc58f
vampirefan <vampirefanmail@gmail.com>
2025-03-13 09:13:44 +0800

    feat: 新增大屏示例；依赖更新

--- src/pages/Demo/DataView.vue
+++ src/pages/Demo/DataView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import dayjs from 'dayjs'
+import { VueUiCarouselTable, type VueUiCarouselTableConfig, type VueUiCarouselTableDataset, VueUiDonut, type VueUiDonutConfig, type VueUiDonutDatasetItem, VueUiDumbbell, type VueUiDumbbellConfig, type VueUiDumbbellDataset, type VueUiXyConfig, type VueUiXyDatasetItem } from 'vue-data-ui'
 
 const screen = ref(null)
 
@@ -9,6 +10,99 @@ const currentDateTime = ref(dayjs().format('YYYY-MM-DD HH:mm:ss'))
 useIntervalFn(() => {
   currentDateTime.value = dayjs().format('YYYY-MM-DD HH:mm:ss')
 }, 1000)
+
+const configXy: VueUiXyConfig = {
+  responsive: true,
+  chart: {
+    userOptions: { show: false },
+    padding: { bottom: 0 },
+    backgroundColor: 'transparent',
+    legend: { color: 'white' },
+    grid: { labels: { color: 'white', xAxisLabels: { show: false } } },
+  },
+}
+const datasetXy: VueUiXyDatasetItem[] = [
+  {
+    name: '数据1',
+    series: Array.from({ length: 50 }, () => (Math.random() * 100)),
+    type: 'bar',
+  },
+  {
+    name: '数据2',
+    series: Array.from({ length: 50 }, () => (Math.random() * -100)),
+    type: 'line',
+    color: 'rgb(66,211,146)',
+
+    dataLabels: false,
+    useArea: true,
+    scaleSteps: 10,
+  },
+  {
+    name: '数据3',
+    series: Array.from({ length: 50 }, (_, i) => (-50 + 10 * Math.E ** (i / 18))),
+    type: 'line',
+    useProgression: true,
+  },
+]
+
+const configTable: VueUiCarouselTableConfig | any = {
+  responsiveBreakpoint: 200,
+  style: { backgroundColor: 'transparent' },
+  userOptions: { show: false },
+  thead: { tr: { height: 50, style: { color: 'white', backgroundColor: 'transparent' } } },
+  tbody: {
+    backgroundColor: 'transparent',
+    tr: {
+      visible: 8,
+      height: 40,
+      style: { color: 'white', backgroundColor: 'transparent' },
+      td: { alternateColor: true, alternateOpacity: 0.1, style: { backgroundColor: '#000A2A' } },
+    },
+
+  },
+  scrollbar: { hide: true },
+}
+const datasetTable: VueUiCarouselTableDataset = {
+  head: ['字段1', '字段2', '字段3'],
+  body: Array.from({ length: 20 }, (_, i) => [String(i + 1), `${String(i + 1)}-1`, `${String(i + 1)}-2`]),
+}
+
+const configDonut: VueUiDonutConfig = {
+  userOptions: { show: false },
+  style: { chart: {
+    color: 'white',
+    backgroundColor: 'transparent',
+    legend: { show: false },
+    layout: {
+      labels: {
+        name: { color: 'white' },
+        percentage: { color: 'white' },
+        hollow: { total: { color: 'white', value: { color: 'white' } }, average: { show: false } },
+      },
+    },
+  } },
+}
+const datasetDonut: VueUiDonutDatasetItem[] = [
+  { name: '数据1', values: [100] },
+  { name: '数据2', values: [200] },
+  { name: '数据3', values: [300, 1] },
+]
+
+const configDumbbell: VueUiDumbbellConfig = {
+  userOptions: { show: false },
+  style: {
+    chart: {
+      backgroundColor: 'transparent',
+      labels: { xAxisLabels: { color: '#cccccc', fontSize: 10 }, yAxisLabels: { color: '#cccccc', fontSize: 10 } },
+      legend: { backgroundColor: 'transparent', color: '#cccccc', labelStart: '2014', labelEnd: '2024' },
+    },
+  },
+}
+const datasetDumbbell: VueUiDumbbellDataset[] = [
+  { name: '数据1', start: 5000, end: 9100 },
+  { name: '数据2', start: 3000, end: 4200 },
+  { name: '数据3', start: 1000, end: 2000 },
+]
 </script>
 
 <template>
@@ -31,23 +125,33 @@ useIntervalFn(() => {
     <!-- 中间行 (60% 高度) -->
     <div class="grid grid-cols-4 m4 mb2 gap-4">
       <div class="data-card">
-        <StatisticNumber class="m4" title="数字展示" unit="（个）" number="0012" />
+        <StatisticNumber class="m4" title="数字展示1" unit="（个）" number="0003" />
+        <StatisticNumber class="m4" title="数字展示2" unit="（个）" number="2025" />
+        <StatisticNumber class="m4" title="数字展示3" unit="（个）" number="2077" />
       </div>
       <div class="data-card col-span-2">
-        模块 2
+        <ClientOnly>
+          <VueUiXy :dataset="datasetXy" :config="configXy" />
+        </ClientOnly>
       </div>
       <div class="data-card">
-        模块 3
+        <ClientOnly>
+          <VueUiCarouselTable :dataset="datasetTable" :config="configTable" />
+        </ClientOnly>
       </div>
     </div>
 
     <!-- 底部行 (40% 高度) -->
     <div class="grid grid-cols-4 m4 mt2 gap-4">
       <div class="data-card col-span-1">
-        模块 4
+        <ClientOnly>
+          <VueUiDonut :dataset="datasetDonut" :config="configDonut" />
+        </ClientOnly>
       </div>
       <div class="data-card col-span-3">
-        模块 5
+        <ClientOnly>
+          <VueUiDumbbell :dataset="datasetDumbbell" :config="configDumbbell" />
+        </ClientOnly>
       </div>
     </div>
   </div>
